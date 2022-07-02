3f5561313c3b4c82966a92de0be7a3cb4724055d
AkshithaKola <102524785+AkshithaKola@users.noreply.github.com>
2022-07-02 12:15:45 +0530

    Add files via upload

---
+++ Codes/1.1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+void uniform(char *str, int len)
+{
+int i;
+FILE *fp;
+
+fp = fopen(str,"w");
+//Generate numbers
+for (i = 0; i < len; i++)
+{
+fprintf(fp,"%lf\n",(double)rand()/RAND_MAX);
+}
+fclose(fp);
+ }
+
+int  main(void) //main function begins
+{
+uniform("uni.dat",1000000);
+printf("asdfghjk");
+return 0;
+}
+
